5e5ee49a24f77f963c323674555ffa61fbb42b9e
mariborges22 <mariborgesdatascientist@gmail.com>
2026-03-03 18:32:44 -0300

    fix: prevent service discovery and s3 recreation

--- infra/modules/networks/main.tf
+++ infra/modules/networks/main.tf
@@ -309,6 +309,10 @@ resource "aws_service_discovery_service" "postgres" {
   }
 
   health_check_custom_config {
-    # failure_threshold is deprecated and no longer supported by AWS
+    # failure_threshold is deprecated
+  }
+
+  lifecycle {
+    ignore_changes = [health_check_custom_config]
   }
 }
